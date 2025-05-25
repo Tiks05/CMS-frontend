670377b94bb417a03ef8f39a661190c31d12f28b
Tiks <tiks39862@gmail.com>
2025-05-25 13:41:25 +0800

    项目结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
+import Login from '@/views/Login/Index.vue'
+import Layout from '@/views/Layout/Index.vue'
 import Home from '@/views/Home/Index.vue'
-import Category from '@/views/Category/List.vue'
+import Category from '@/views/Category/Index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
